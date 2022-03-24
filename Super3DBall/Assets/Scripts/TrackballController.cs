bc706f92ade16d954b1d963f7d171644d97a85dd
antronik7 <antoineberthiaumedutil@gmail.com>
2022-03-23 21:26:10 -0400

    Rolling Mechanic
    -Added a basic manipulation of the Trackball to make the Ball roll.

--- Super3DBall/Assets/Scripts/TrackballController.cs
+++ Super3DBall/Assets/Scripts/TrackballController.cs
@@ -9,6 +9,12 @@ public class TrackballController : MonoBehaviour
     public Camera guiCamera;
     private BallController ball;
 
+    //Variables
+    private bool tochingTrackball = false;
+    private bool canAddForce = false;
+    private Vector3 previousTouchPosition;
+    private Vector3 currentTouchPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,26 +25,67 @@ public class TrackballController : MonoBehaviour
     void Update()
     {
         CheckInputs();
+
+        if (canAddForce)
+            UpdateTrackballForce();
+
         CopyRotation(ballTransform.rotation);
     }
 
     void CheckInputs()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && IsTochingTrackball())
+            OnStartTochingTrackball();
+        else if (tochingTrackball && (Input.GetMouseButtonUp(0) || IsTochingTrackball() == false))
+            OnStopTochingTrackball();
+
+        if(canAddForce)
         {
-            Vector3 trackBallScreenPosition = guiCamera.WorldToScreenPoint(transform.position);
-            Vector3 trackBallRadiusScreenPosition = guiCamera.WorldToScreenPoint(transform.position + (Vector3.forward * (transform.localScale.x / 2.0f)));
-
-            float touchRadius = (trackBallRadiusScreenPosition - trackBallScreenPosition).sqrMagnitude;
-            float mouseMagnitude = (Input.mousePosition - trackBallScreenPosition).sqrMagnitude;
-
-            if (mouseMagnitude <= touchRadius)
-                Debug.Log("Touch Detected");
+            previousTouchPosition = currentTouchPosition;
+            currentTouchPosition = Input.mousePosition;
         }
+    }
+
+    void UpdateTrackballForce()
+    {
+        if (tochingTrackball == false)
+            canAddForce = false;
+
+        Vector3 trackBallRadiusScreenPosition = guiCamera.WorldToScreenPoint(transform.position + (Vector3.forward * (transform.localScale.x / 2.0f)));
+        Vector3 dragVector = currentTouchPosition - previousTouchPosition;
+        //float dragForce = (dragVector.magnitude / trackBallRadiusScreenPosition.magnitude) / Time.deltaTime;
+        dragVector = new Vector3(dragVector.x, 0f, dragVector.y);
+        ball.GiveForce(dragVector);
     }
 
     void CopyRotation(Quaternion rotation)
     {
         transform.rotation = rotation;
+    }
+
+    void OnStartTochingTrackball()
+    {
+        tochingTrackball = true;
+        canAddForce = true;
+        currentTouchPosition = Input.mousePosition;
+    }
+
+    void OnStopTochingTrackball()
+    {
+        tochingTrackball = false;
+    }
+
+    bool IsTochingTrackball()
+    {
+        Vector3 trackBallScreenPosition = guiCamera.WorldToScreenPoint(transform.position);
+        Vector3 trackBallRadiusScreenPosition = guiCamera.WorldToScreenPoint(transform.position + (Vector3.forward * (transform.localScale.x / 2.0f)));
+
+        float touchRadius = (trackBallRadiusScreenPosition - trackBallScreenPosition).sqrMagnitude;
+        float mouseMagnitude = (Input.mousePosition - trackBallScreenPosition).sqrMagnitude;
+
+        if (mouseMagnitude <= touchRadius)
+            return true;
+
+        return false;
     }
 }
